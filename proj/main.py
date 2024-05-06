e1d58da88a2ea5749f11661866079ead935c946d
mvhhampek <59012814+mvhhampek@users.noreply.github.com>
2024-05-06 15:18:38 +0200

    Delete main.py

--- proj/main.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-import torch.optim as optim
-from torch.utils.data import DataLoader, random_split
-from torch.utils.tensorboard import SummaryWriter
-import copy
